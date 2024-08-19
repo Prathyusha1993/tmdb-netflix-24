517bcb30b0d27890d10e28822e986ebe8eddca0a
Prathyusha Kurigala <prathyushakiran@Prathyushas-MBP.attlocal.net>
2024-08-19 11:42:12 -0700

    padding adjusted to movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -17,12 +17,12 @@ const MovieCard = ({posterPath, title, overview, releaseDate, IMDBRating, backdr
           <div className='absolute left-0 right-0 m-auto bg-sky-400 hover:bg-sky-700 w-[30%] transform-gpu scale-150 text-white rounded-lg'>
             <button onClick={() => setShowDetails(!showDetails)} className='float-right mr-2'>X</button>
             <img src={IMG_CDN_URL+ backdropPath} />
-            <h2 className='font-bold'>{title}, Rating:{IMDBRating} </h2>
+            <h2 className='p-2 font-bold'>{title}, Rating:{IMDBRating} </h2>
             <div>
-              <button className='bg-white text-black py-1 md:py-0 px-3 md:px-4 text-sm rounded-lg hover:bg-opacity-80'> ▶️Play</button>
-              <button className='bg-white ml-1 text-black py-1 md:py-0 px-3 md:px-4 text-sm rounded-lg hover:bg-opacity-80'> ⬇️Download</button>
+              <button className='p-2 bg-white text-black py-1 md:py-0 px-3 md:px-4 text-sm rounded-lg hover:bg-opacity-80'> ▶️Play</button>
+              <button className='p-2 bg-white ml-1 text-black py-1 md:py-0 px-3 md:px-4 text-sm rounded-lg hover:bg-opacity-80'> ⬇️Download</button>
             </div>
-            <p className='text-sm'>{overview}</p>
+            <p className='text-sm p-2'>{overview}</p>
             {/* <h3>{releaseDate}</h3> */}
           </div>
         )}
